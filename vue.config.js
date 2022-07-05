b3351e82f06b9ca30db1f208a01e7849805cb828
jinweiOS <jinwei.peng@outlook.com>
2022-07-05 15:42:04 +0800

    feat: 修改配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/vue-github-page/'
+  publicPath: '/vue-github-page/',
+  lintOnSave: false
 })
